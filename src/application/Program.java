a373a06d97aca99edcb1fa63489c6b3eaa15cb8c
Rafael Moraes <rafael.msantos@sptech.school>
2023-03-31 11:53:19 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,20 @@
 package application;
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
 public class Program {
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-
+		
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
@@ -30,11 +29,17 @@ public class Program {
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-
+				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+				
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
